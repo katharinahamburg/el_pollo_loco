ffd133a76469fa6f8fc2174ef03a9cbec7951769
katharinahamburg <katharina.hinnrichs@gmx.net>
2023-07-03 08:44:57 +0200

    hurt function and images

--- models/character.class.js
+++ models/character.class.js
@@ -26,8 +26,23 @@ class Character extends MovableObject {
         'img/2_character_pepe/3_jump/J-38.png',
         'img/2_character_pepe/3_jump/J-39.png',
         'img/2_character_pepe/3_jump/J-39.png',
-        'img/2_character_pepe/3_jump/J-39.png',
-        
+        'img/2_character_pepe/3_jump/J-39.png'      
+    ];  
+
+    IMAGES_DEAD = [
+        'img/2_character_pepe/5_dead/D-51.png',
+        'img/2_character_pepe/5_dead/D-52.png',
+        'img/2_character_pepe/5_dead/D-53.png',
+        'img/2_character_pepe/5_dead/D-54.png',
+        'img/2_character_pepe/5_dead/D-55.png',
+        'img/2_character_pepe/5_dead/D-56.png',
+        'img/2_character_pepe/5_dead/D-57.png'   
+    ];  
+
+    IMAGES_HURT = [
+        'img/2_character_pepe/4_hurt/H-41.png',
+        'img/2_character_pepe/4_hurt/H-42.png',
+        'img/2_character_pepe/4_hurt/H-43.png'   
     ];  
 
     world;
@@ -37,9 +52,11 @@ class Character extends MovableObject {
 
 
     constructor(){
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_JUMPING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
         this.applyGravity();
         this.animate();
     }
@@ -48,21 +65,21 @@ class Character extends MovableObject {
 
         setInterval(() => {
             this.walking_sound.pause();
-            if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){ 
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){ 
                 this.moveRight();
                 this.otherDirection = false;
                 this.walking_sound.play();
             }
 
-            if(this.world.keyboard.LEFT && this.x > 0){ 
-               this.moveLeft();
-               this.otherDirection = true;
+            if (this.world.keyboard.LEFT && this.x > 0){ 
+                this.moveLeft();
+                this.otherDirection = true;
                 this.walking_sound.play();
             }
 
             
 
-            if(this.world.keyboard.SPACE && !this.isAboveGround()){
+            if (this.world.keyboard.SPACE && !this.isAboveGround()){
                this.jump();
                this.jump_sound.play();
            }  
@@ -73,24 +90,23 @@ class Character extends MovableObject {
 
         setInterval(() => { 
 
-            if(this.isAboveGround()) {
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+            } else if (this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+            } else if (this.isAboveGround()) {
                 this.playAnimation(this.IMAGES_JUMPING);
             } else {
-
-
-        if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){ 
-            this.playAnimation(this.IMAGES_WALKING);
-
-            //walk animation
-        
-        }
-    }
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT){ 
+            this.playAnimation(this.IMAGES_WALKING);  
+            } 
+            }
     }, 50);
     }
 
     jump(){
-        this.speedY = 20  ;
+        this.speedY = 20;
     }
     
     
-}
+    }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -9,6 +9,8 @@ class MovableObject {
     otherDirection = false;
     speedY = 0;
     acceleration = 1;
+    energy = 1000;
+    lastHit = 0;
 
 
     applyGravity(){
@@ -41,7 +43,7 @@ class MovableObject {
         if(this instanceof Character || this instanceof Chicken || this instanceof Endboss){
         ctx.beginPath();
         ctx.lineWidth = '3';
-        ctx.strokeStyle = 'blue';
+        ctx.strokeStyle = 'transparent';
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.stroke();
     }
@@ -78,14 +80,44 @@ class MovableObject {
             this.speedY = 0;
         }
     
+
+        // character.isColliding(chicken);
+        isColliding(mo) {
+            return this.x + this.width > mo.x &&
+                this.y + this.height > mo.y &&
+                this.x < mo.x + mo.width &&
+                this.y < mo.y + mo.height;
+        }
+
+        hit() {
+            this.energy -= 5;
+            if (this.energy < 0) {
+                this.energy = 0;
+            } else { 
+                this.lastHit = new Date().getTime();
+
+            }
+        }
+
+        isHurt() {
+            let timepassed = new Date().getTime() - this.lastHit;
+            timepassed = timepassed / 1000;
+            return timepassed < 1;
+        }
+
+        isDead() {
+            return this.energy == 0;
+        }
+
+        
     }
 
     
 
     /*
-    isColliding(mo) {
-        return this.x + this.width > mo.x &&
-            this.y + this.height > mo.y &&
-            this.x < mo.x + mo.width &&
-            this.y < mo.y + mo.height;
-    }*/
+    if (character.x + chracter.width > chicken.x &&
+        character.y + character.height > chickeny &&
+        character.x < chicken.x &&
+        character.y < chicken.y + chicken.height
+        )*/
+    
